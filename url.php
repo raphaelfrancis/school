b688b3aafd5e12a0d49c9cf6b511b7792c8c767d
raphaelfrancis <raphaelfrancis2002@gmail.com>
2018-07-13 07:39:10 +0530

    one new file added
    URL FILE ADDED

---
+++ url.php
@@ -0,0 +1,9 @@
+<html>
+<head><h1><center>CHECKING URL</center></h1></head>
+<body>
+<form name="f1" method="post" action="urlaction.php">
+ADD URL:<input type="text" name="t1">
+<input type ="submit" value ="submit" name ="submit">
+</form>
+</body>
+</html>
